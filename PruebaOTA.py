3414dc6eba7a81e2c03edbff09b0380a8457dc36
LMario28 <LMario28@users.noreply.github.com>
2024-07-13 20:23:32 -0600

    Update PruebaOTA.py

--- PruebaOTA.py
+++ PruebaOTA.py
@@ -1,9 +1,9 @@
 from ota import OTAUpdater
 from WIFI_CONFIG import SSID, PASSWORD
 
-firmware_url = "https://raw.githubusercontent.com/LMario28/Marquesina/"
+firmware_url = "https://raw.githubusercontent.com/LMario28/Marquesina/main/"
 
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "main.py")
+ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "PruebaOTA.py")
 ota_updater.download_and_install_update_if_available()
 
 print("Uno")
