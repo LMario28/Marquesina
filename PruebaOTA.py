bf4fbbd7e3135441207d90e4767ed078814551d0
LMario28 <LMario28@users.noreply.github.com>
2026-03-22 01:44:11 -0600

    Delete PruebaOTA.py

--- PruebaOTA.py
+++
@@ -1,9 +0,0 @@
-from ota import OTAUpdater
-from WIFI_CONFIG import SSID, PASSWORD
-
-firmware_url = "https://raw.githubusercontent.com/LMario28/Marquesina/"
-
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "PruebaOTA.py")
-ota_updater.download_and_install_update_if_available()
-
-print("Prueba DOS")
